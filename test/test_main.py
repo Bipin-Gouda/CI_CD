9b20fae5e8c5df52bb05c58d4006059f6c70d2d0
Bipin-Gouda <bipingouda1199@gmail.com>
2025-04-07 19:46:22 +0530

    updated test file

--- test/test_main.py
+++ test/test_main.py
@@ -1,3 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# added these to include parent directory in the path
+
+
 from fastapi.testclient import TestClient
 from main import app
 
